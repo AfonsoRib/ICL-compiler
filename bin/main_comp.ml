03b84e7c76623767d05048b7ad8f16dae0876c74
AfonsoRib <afonsoviegas0@gmail.com>
2024-04-29 14:26:48 +0100

    create comp executable

---
+++ bin/main_comp.ml
@@ -0,0 +1,45 @@
+open Icl
+
+let footer = "invokestatic java/lang/String/valueOf(I)Ljava/lang/String;
+invokevirtual java/io/PrintStream/println(Ljava/lang/String;)V
+return
+.end method
+"
+
+let preamble = ".class public Demo
+.super java/lang/Object
+.method public <init>()V
+   aload_0
+   invokenonvirtual java/lang/Object/<init>()V
+   return
+.end method
+.method public static main([Ljava/lang/String;)V
+ .limit locals 10
+ .limit stack 256
+ ; setup local variables:
+ ;    1 - the PrintStream object held in java.lang.out
+getstatic java/lang/System/out Ljava/io/PrintStream;
+"
+
+
+let main2 =
+  let filename = "test.txt" in (* Specify your input file name here *)
+  let channel = open_in filename in
+  let outputname = "jasmin.j" in
+  let outchannel = open_out outputname in
+  try
+    let lexbuf = Lexing.from_channel channel in
+    let ast = Parser.start Lexer.token lexbuf in
+    let typecheck = Typechecker.typechecker ast (ref None) in
+    if typecheck = Typechecker.NoneType then failwith "Failed typechecker pass. Expression type: None";
+    let res = Comp.comp ast in
+    output_string outchannel preamble;
+    List.iter (fun x -> output_string outchannel ((Comp.jvmString x) ^ "\n")) res;
+    output_string outchannel footer;
+  with | Failure msg -> print_endline msg
+       | _ -> print_endline "Syntax error!"
+
+
+
+let () =
+  main2
---
+++ lib/comp.ml
@@ -0,0 +1,78 @@
+open Ast
+
+let counter = ref 0
+
+let gen_number () =
+  let fresh_number = !counter in
+  counter := fresh_number + 1;
+  fresh_number
+
+type jvm =
+  | Iadd 
+  | Isub 
+  | Imul 
+  | Idiv 
+  | If_icmpeq of string
+  | If_icmpne of string
+  | If_icmpge of string
+  | If_icmpgt of string
+  | If_icmple of string
+  | If_icmplt of string
+  | Sipush of int
+  | Iconst_1
+  | Iconst_0
+  | Istore_0
+  | Istore_1
+  | Label of string * jvm
+  | Nop
+  | Goto of string
+ 
+
+let rec jvmString = function
+  | Iadd -> "iadd"
+  | Isub -> "isub"
+  | Imul -> "imul"
+  | Idiv -> "idiv"
+  | If_icmpeq label -> "if_icmpeq " ^ label
+  | If_icmpne label -> "if_icmpne " ^ label
+  | If_icmpge label -> "if_icmpge " ^ label
+  | If_icmpgt label -> "if_icmpgt " ^ label
+  | If_icmple label -> "if_icmple " ^ label
+  | If_icmplt label -> "if_icmplt " ^ label
+  | Sipush x -> "sipush " ^ string_of_int x
+  | Iconst_1 -> "iconst_1"
+  | Iconst_0 -> "iconst_0"
+  | Istore_0 -> "istore_0"
+  | Istore_1 -> "istore_1"
+  | Label (label,inst) ->  label ^ ":\n" ^ (jvmString inst)
+  | Goto label -> "goto " ^ label
+  | Nop -> "nop"
+
+
+let rec comp (expression : exp) (* (environment : env) *) : jvm list =
+  match expression with
+  | Fact n -> [Sipush n]
+  | Statement b -> if b then [Sipush 1] else [Sipush 0]
+  | Add (e1, e2) -> comp e1 @ comp e2 @ [Iadd]
+  | Mult (e1, e2) -> comp e1 @ comp e2 @ [Imul]
+  | Sub (e1, e2) -> comp e1 @ comp e2 @ [Isub]
+  | Div (e1, e2) -> comp e1 @ comp e2 @ [Idiv]
+  | Eq (e1, e2) -> let l1 = "L" ^ (string_of_int (gen_number ())) in
+                   let l2 = "L" ^ (string_of_int (gen_number ())) in
+                   comp e1 @ comp e2 @ [If_icmpne l1 ; Sipush 1; Goto l2; Label (l1,Sipush 0); Label (l2, Nop)]
+  | Ne (e1, e2) -> let l1 = "L" ^ (string_of_int (gen_number ())) in
+                   let l2 = "L" ^ (string_of_int (gen_number ())) in
+                   comp e1 @ comp e2 @ [If_icmpeq l1 ; Sipush 1; Goto l2; Label (l1,Sipush 0); Label (l2, Nop)]
+  | Le (e1, e2) -> let l1 = "L" ^ (string_of_int (gen_number ())) in
+                   let l2 = "L" ^ (string_of_int (gen_number ())) in
+                   comp e1 @ comp e2 @ [If_icmpgt l1 ; Sipush 1; Goto l2; Label (l1,Sipush 0); Label (l2, Nop)]
+  | Ge (e1, e2) -> let l1 = "L" ^ (string_of_int (gen_number ())) in
+                   let l2 = "L" ^ (string_of_int (gen_number ())) in
+                   comp e1 @ comp e2 @ [If_icmplt l1 ; Sipush 1; Goto l2; Label (l1,Sipush 0); Label (l2, Nop)]
+  | Lt (e1, e2) -> let l1 = "L" ^ (string_of_int (gen_number ())) in
+                   let l2 = "L" ^ (string_of_int (gen_number ())) in
+                   comp e1 @ comp e2 @ [If_icmpge l1 ; Sipush 1; Goto l2; Label (l1,Sipush 0); Label (l2, Nop)]
+  | Gt (e1, e2) -> let l1 = "L" ^ (string_of_int (gen_number ())) in
+                   let l2 = "L" ^ (string_of_int (gen_number ())) in
+                   comp e1 @ comp e2 @ [If_icmple l1 ; Sipush 1; Goto l2; Label (l1,Sipush 0); Label (l2, Nop)]
+  | _ -> [Nop]
